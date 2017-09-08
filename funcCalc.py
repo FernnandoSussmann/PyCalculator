1db247edfa5df7f6e7677d3591c124a4a8b96ae3
Fernnando Sussmann <fernnandoh@hotmail.com>
2017-09-08 13:18:11 -0300

    Creation of a version using lambdas

---
+++ funcCalc.py
@@ -0,0 +1,5 @@
+lambs = [lambda x,y: x + y,lambda x,y: x - y,lambda x,y: x * y,lambda x,y: x / y,lambda x,y: x ** y,lambda x,y: x ** 1/y]
+opt = int(raw_input("Select an option: 0 = +, 1 = -, 2 = *, 3 = /, 4 = **, 5 = root:"))
+num = float(raw_input("First number:"))
+num2 = float(raw_input("Second number:"))
+print lambs[opt](num,num2)
