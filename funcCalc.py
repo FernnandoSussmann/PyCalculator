ab0cd211213995d69d33e8e0ea32bd2b7b24b8cd
Fernnando Sussmann <fernnandoh@hotmail.com>
2017-09-14 00:38:57 -0300

    Fix on the root calculation of funcCalc.py file

--- funcCalc.py
+++ funcCalc.py
@@ -1,4 +1,4 @@
-lambs = [lambda x,y: x + y,lambda x,y: x - y,lambda x,y: x * y,lambda x,y: x / y,lambda x,y: x ** y,lambda x,y: x ** 1/y]
+lambs = [lambda x,y: x + y,lambda x,y: x - y,lambda x,y: x * y,lambda x,y: x / y,lambda x,y: x ** y,lambda x,y: x ** (1/y)]
 opt = int(raw_input("Select an option: 0 = +, 1 = -, 2 = *, 3 = /, 4 = **, 5 = root:"))
 num = float(raw_input("First number:"))
 num2 = float(raw_input("Second number:"))
